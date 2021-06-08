346e01294d66dd4cc591263ab6346d7c751f85b5
En1ggma <85566287+En1ggma@users.noreply.github.com>
2021-06-08 17:15:27 +0300

    Первая версия сайта на django

---
+++ users/forms.py
@@ -0,0 +1,24 @@
+from users.models import Profile
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class OurRegistration(UserCreationForm):
+    email = forms.EmailField()
+
+    class META:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileAvatar(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ['avatar']
